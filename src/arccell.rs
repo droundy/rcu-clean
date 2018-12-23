079be2176b52f79856cb239e3e2c87689179402a
David Roundy <roundyd@physics.oregonstate.edu>
2018-12-23 13:10:35 -0800

    split out and break ArcCell code

--- src/arccell.rs
+++ src/arccell.rs
@@ -1,3 +1,6 @@
+use std::cell::Cell;
+use std::sync::atomic::{AtomicPtr, Ordering};
+use std::sync::{Arc, Mutex};
 /// A thread-safe reference counted pointer that allows interior mutability
 ///
 /// An [ArcCell] is currently the size of a five pointers and has an
@@ -15,24 +18,39 @@
 /// assert_eq!(*x, 7); // but the pointer now points to the new value.
 /// assert_eq!(*z, 7); // but the cloned pointer also points to the new value.
 /// ```
-
-#[derive(Clone)]
 pub struct ArcCell<T> {
-    inner: Arc<UnsafeCell<BoxCellInner<T>>>,
-    have_borrowed: Cell<bool>,
+    inner: Arc<Inner<T>>,
 }
-// unsafe impl<T: Send + Clone> Send for ArcCell<T> {}
-// unsafe impl<T: Sync + Clone> Sync for ArcCell<T> {}
+impl<T: Clone> Clone for ArcCell<T> {
+    fn clone(&self) -> Self {
+        ArcCell {
+            inner: self.inner.clone(),
+        }
+    }
+}
+pub struct Inner<T> {
+    current: AtomicPtr<T>,
+    old: Mutex<Vec<Box<T>>>,
+    borrow_count: AtomicUsize,
+}
+impl<T> Drop for Inner<T> {
+    fn drop(&mut self) {
+        // this frees the current value of the pointer.  It is acquire
+        // because the contents of the pointer must be up to date so
+        // the drop of T doesn't do anything crazy.
+        unsafe { Box::from_raw(self.current.load(Ordering::Acquire)); }
+    }
+}
+
 
 impl<T: Clone> ArcCell<T> {
     pub fn new(value: T) -> ArcCell<T> {
         ArcCell {
-            inner: Arc::new(UnsafeCell::new(BoxCellInner {
-                current: Box::new(value),
-                old: Vec::new(),
-                borrow_count: 0,
-            })),
-            have_borrowed: Cell::new(false),
+            inner: Arc::new(Inner {
+                current: AtomicPtr::new(Box::into_raw(Box::new(value))),
+                old: Mutex::new(Vec::new()),
+                borrow_count: AtomicUsize::new(0),
+            }),
         }
     }
     /// Make a copy of the data and return a reference.
@@ -41,41 +59,44 @@ impl<T: Clone> ArcCell<T> {
     /// no protection against two simultaneous updates.  The one that
     /// drops second will "win".
     pub fn update<'a>(&'a self) -> impl 'a + std::ops::DerefMut<Target=T> {
-        unsafe {
-            Guard {
-                value: Box::new((*(*self)).clone()),
-                inner: &mut *self.inner.get(),
-            }
-        }
+        let g: Guard<'a,T> = Guard {
+            value: Box::into_raw(Box::new((*(*self)).clone())),
+            boxcell: self,
+            guard: self.inner.old.lock().unwrap(),
+        };
+        g
     }
-    /// Free all old versions of the data.  Because this method
-    /// requires a mutable reference, it is guaranteed that no
-    /// references exist.
+    /// Free all old versions of the data if possible.  Because this
+    /// method requires a mutable reference, it is guaranteed that no
+    /// references exist to *this* ArcCell, but we don't know about
+    /// other ArcCells.  We could track borrows, but instead we simply restrict ourselves t
     pub fn clean(&mut self) {
+        if self.inner.strong_count() == 1 {
+            // We
+        }
         if self.have_borrowed.get() {
-            self.have_borrowed.set(false);
-            unsafe {
-                let mut inner = &mut *self.inner.get();
-                inner.borrow_count -= 1;
-                if inner.borrow_count == 0 {
-                    inner.old = Vec::new();
-                }
+            self.inner.borrow_count.set(self.inner.borrow_count.get() - 1);
+            if self.inner.borrow_count.get() == 0 {
+                *self.inner.old.borrow_mut() = Vec::new();
             }
+            self.have_borrowed.set(false);
         }
     }
 }
-
 impl<T> std::ops::Deref for ArcCell<T> {
     type Target = T;
     fn deref(&self) -> &T {
         let aleady_borrowed = self.have_borrowed.get();
-        self.have_borrowed.set(true); // indicate we have borrowed this once.
+        if !aleady_borrowed {
+            self.inner.borrow_count.set(self.inner.borrow_count.get() + 1);
+            self.have_borrowed.set(true); // indicate we have borrowed this once.
+        }
         unsafe {
-            let mut inner = &mut *self.inner.get();
-            if !aleady_borrowed {
-                inner.borrow_count += 1;
-            }
-            &inner.current
+            // I think I need to Acquire here because otherwise it may
+            // be possible to update a pointer and then have the new
+            // pointer value visible when the bytes that are pointed
+            // to are still incorrect.
+            &*self.inner.current.load(Ordering::Acquire)
         }
     }
 }
@@ -86,32 +107,25 @@ impl<T> std::borrow::Borrow<T> for ArcCell<T> {
     }
 }
 
-pub struct BoxCellInner<T> {
-    current: Box<T>,
-    old: Vec<Box<T>>,
-    borrow_count: usize,
-}
 struct Guard<'a,T: Clone> {
-    value: Box<T>,
-    inner: &'a mut BoxCellInner<T>,
+    value: *mut T,
+    boxcell: &'a ArcCell<T>,
+    guard: std::sync::MutexGuard<'a,Vec<Box<T>>>,
 }
 impl<'a,T: Clone> std::ops::Deref for Guard<'a,T> {
     type Target = T;
     fn deref(&self) -> &T {
-        &*self.value
+        unsafe { &*self.value }
     }
 }
 impl<'a,T: Clone> std::ops::DerefMut for Guard<'a,T> {
     fn deref_mut(&mut self) -> &mut T {
-        &mut *self.value
+        unsafe { &mut *self.value }
     }
 }
 impl<'a,T: Clone> Drop for Guard<'a,T> {
     fn drop(&mut self) {
-        // FIXME I'd like to avoid the needless clone here.  Do I
-        // really need to use an Option<Box<T>> just to avoid
-        // allocating something to destroy?
-        self.inner.old.push(std::mem::replace(&mut self.inner.current,
-                                              self.value.clone()));
+        self.value = self.boxcell.inner.current.swap(self.value, Ordering::Release);
+        self.guard.push(unsafe { Box::from_raw(self.value) });
     }
 }
