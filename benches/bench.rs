8a8ab2551616c3c3b49380374f55631ded286d09
David Roundy <roundyd@physics.oregonstate.edu>
2018-12-23 21:18:31 -0800

    change bench to just use 1000 elements

--- benches/bench.rs
+++ benches/bench.rs
@@ -246,7 +246,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     }));
 
     funs.reverse();
-    c.bench_functions("sum", funs, 1<<10);
+    c.bench_functions("sum", funs, 1000);
 }
 
 criterion_group!(benches, criterion_benchmark);
